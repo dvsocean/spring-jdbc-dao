e10e29178ba665d2d307434dec6675dec4cdda02
Ocean <dvsocean@icloud.com>
2018-07-18 15:55:02 -0700

    DaoUtils in progess

--- src/main/java/jdbcdao/utilities/DaoUtils.java
+++ src/main/java/jdbcdao/utilities/DaoUtils.java
@@ -1,7 +1,52 @@
 package jdbcdao.utilities;
 
+import java.util.ArrayList;
+import java.util.List;
+import jdbcdao.dao.OrganizationDAO;
+import jdbcdao.org.Organization;
+
 public class DaoUtils {
 
+  public static final String CREATE_OPERATION = "CREATE";
+  public static final String READ_OPERATION = "READ";
+  public static final String UPDATE_OPERATION = "UPDATE";
+  public static final String DELETE_OPERATION = "DELETE";
+  public static final String CLEANUP_OPERATION = "TRUNCATE";
 
+  public static void printOrganizations(List<Organization> orgsList, String operation){
+    System.out.println("\n****************** printing organizations after " + operation + " operation **************");
+    for(Organization org: orgsList){
+      System.out.println(org);
+    }
+  }
 
-}
+  public static void printSuccessFailure(String opertion, boolean param){
+    if(param){
+      System.out.println("\nOperation " + opertion + " successful!");
+    } else {
+      System.out.println("\nOperation " + opertion + " failed!");
+    }
+  }
+
+  public static void createSeedData(OrganizationDAO dao){
+    Organization org1 = new Organization("Amazon", 1998, "91789", 1200, "Work hard, have fun and make history");
+    Organization org2 = new Organization("GoodsByeOcean", 2016, "95691", 5500, "I struggled to make it now it pays off");
+    Organization org3 = new Organization("NerdSchool", 2015, "91768", 1, "Blood sweat and tears at this school and now im here");
+
+    List<Organization> orgs = new ArrayList<Organization>();
+    orgs.add(1, org1);
+    orgs.add(2, org2);
+    orgs.add(3, org3);
+
+    int createCount = 0;
+    for(Organization org: orgs){
+      boolean isCreated = dao.create(org);
+      if(isCreated){
+        createCount += 1;
+      }
+      System.out.println("Created " + createCount + " organizations");
+    }
+
+  }
+
+}//End of class
